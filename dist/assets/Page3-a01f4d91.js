ed044d5dd2adcdb563da5301977233555c260cde
Jayce <1102408100@qq.com>
2023-10-10 10:45:55 +0800

    feat: adding dist dirctory

---
+++ dist/assets/Page3-a01f4d91.js
@@ -0,0 +1,1 @@
+import{B as e}from"./ChinaMap.vue_vue_type_style_index_0_scoped_8531e1fc_lang-4b60816a.js";import{_ as a,a as _,c as s,d as t,u as o}from"./index-93e7ffd7.js";const c={class:"page3"},r={__name:"Page3",setup(n){return(p,d)=>(_(),s("div",c,[t(o(e))]))}},l=a(r,[["__scopeId","data-v-f752da65"]]);export{l as default};
